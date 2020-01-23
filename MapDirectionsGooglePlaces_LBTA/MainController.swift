34a4659df3c3ddffb7d6bfedf842904d61586346
Frank Cipolla <frankc@cscit.net>
2020-01-23 09:30:25 -0500

    Finished Ch_02

--- MapDirectionsGooglePlaces_LBTA/MainController.swift
+++ MapDirectionsGooglePlaces_LBTA/MainController.swift
@@ -8,29 +8,83 @@
 
 import UIKit
 import MapKit
+import SwiftUI
+
 import LBTATools
 
 class MainController: UIViewController {
+    
+    let mapView = MKMapView() // now using auto layout
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        let mapView = MKMapView(frame: view.frame)
-        let mapView = MKMapView() // now using auto layout
+        
         
         view.addSubview(mapView)
         
         // Use LBTATools
-        mapView.fillSuperview()
+        mapView.fillSuperview() // replaces code below:
         
-        mapView.mapType = .satellite
+        //        mapView.translatesAutoresizingMaskIntoConstraints = false // enable auto layout
+        //        // set constraints
+        //        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        //        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        //        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        //        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
-//        mapView.translatesAutoresizingMaskIntoConstraints = false // enable auto layout
-//        // set constraints
-//        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-
-//        view.backgroundColor = .yellow
+        // MapView Options:
+        //        view.backgroundColor = .yellow
+       // mapView.mapType = .satellite // set map type
+        
+        setupRegionForMap()
     }
+// MARK: Set Map Region
+        
+        
+        
+        fileprivate func setupRegionForMap() {
+            let centerCoordinate = CLLocationCoordinate2D(latitude: 34.091708, longitude: -84.590092)
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            let region = MKCoordinateRegion(center: centerCoordinate, span: span)
+            mapView.setRegion(region, animated: true)
+
+    }
+    
+    // MARK: SwiftUI Preview
+  
+    
+    struct MainPreview: PreviewProvider {
+        static var previews: some View {
+           // Text("Main preview HERE")
+        // insert containerview here
+            ContainerView().edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    // MARK: Imbed MapView into preview pane
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainController.ContainerView>) -> MainController {
+            return MainController() // wants desired controller
+        }
+        
+        func updateUIViewController(_ uiViewController: MainController, context: UIViewControllerRepresentableContext<MainController.ContainerView>) {
+        }
+        
+        
+    
+        typealias UIViewControllerType = MainController
+        
+        
+        }
+    
+    
+    
+    
+    
+    
+    
 }
